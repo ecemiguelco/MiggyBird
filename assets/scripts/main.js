0d2e65ae2e1d4ec95467a85bab14f2b47509231f
Miguel Lorenzo Co <ece.miguel.co@gmail.com>
2023-05-29 22:51:37 +0800

    fix: game perfomance/speed changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,8 +10,8 @@ const playAgainBtn = document.querySelector(".play-again-btn");
 const resetDataBtn = document.querySelector(".reset-data-btn");
 const flyBtn = document.querySelector(".fly-button");
 
-const gravity = 0.25;
-let speed = 3;
+const gravity = 0.65;
+let speed = 7;
 let gameDificulty = 1;
 let score = 0;
 let highScore = 0;
@@ -24,198 +24,198 @@ playAgainBtn.addEventListener("click", restartGame);
 resetDataBtn.addEventListener("click", resetData);
 
 function resetData() {
-	window.location.reload();
+  window.location.reload();
 }
 
 function restartGame() {
-	game = "Start";
-	speed = 3;
-	gameDificulty = 1;
-	score = 0;
-	scoreBox.innerHTML = `Score: ${score}`;
-	bird.style.top = 116 + "px";
-	birdSpecs = bird.getBoundingClientRect();
-	document.querySelector(".start-prompt").innerHTML = "Press Spacebar / Touch to Start";
-	playAgainBtn.classList.add("hidden");
-	resetDataBtn.classList.add("hidden");
-	document.addEventListener("keydown", ignitionFunction, { once: true });
-	// console.log(game);
-	// console.log(score);
-	// ignitionFunction();
+  game = "Start";
+  speed = 7;
+  gameDificulty = 1;
+  score = 0;
+  scoreBox.innerHTML = `Score: ${score}`;
+  bird.style.top = 116 + "px";
+  birdSpecs = bird.getBoundingClientRect();
+  document.querySelector(".start-prompt").innerHTML = "Press Spacebar / Touch to Start";
+  playAgainBtn.classList.add("hidden");
+  resetDataBtn.classList.add("hidden");
+  document.addEventListener("keydown", ignitionFunction, { once: true });
+  // console.log(game);
+  // console.log(score);
+  // ignitionFunction();
 }
 
 function ignitionFunction() {
-	if (game == "Over") {
-		return;
-	}
-	if (game != "Play") {
-		document.querySelectorAll(".pipes").forEach((e) => {
-			e.remove();
-		});
-		game = "Play";
-		bird.classList.remove("hidden");
-		playGame();
-		console.log(game);
-	}
+  if (game == "Over") {
+    return;
+  }
+  if (game != "Play") {
+    document.querySelectorAll(".pipes").forEach((e) => {
+      e.remove();
+    });
+    game = "Play";
+    bird.classList.remove("hidden");
+    playGame();
+    console.log(game);
+  }
 }
 
 function playGame() {
-	playAgainBtn.classList.add("hidden");
-	flyBtn.classList.remove("hidden");
-	document.querySelector(".start-prompt").classList.add("hidden");
-	let pipeOpening = 30;
-	let pipeInterval = 0;
-	let pipeDistance = 250;
+  playAgainBtn.classList.add("hidden");
+  flyBtn.classList.remove("hidden");
+  document.querySelector(".start-prompt").classList.add("hidden");
+  let pipeOpening = 30;
+  let pipeInterval = 0;
+  let pipeDistance = 250;
 
-	/////////////////////////////////
+  /////////////////////////////////
 
-	function createPipes() {
-		let gameDificultyforDistance = gameDificulty * 10;
-		let pipeDistanceReference = pipeDistance - gameDificultyforDistance;
-		console.log(pipeDistanceReference);
-		if (game != "Play") return;
-		if (pipeInterval > pipeDistanceReference) {
-			console.log(gameDificulty);
-			pipeInterval = 0;
-			let randomNum = Math.floor(Math.random() * 43) + 8;
-			// console.log(randomNum);
-			let pipePositionY = randomNum;
-			let pipeNewInverted = document.createElement("img");
-			pipeNewInverted.setAttribute("src", "assets/images/pipe-green.png");
-			pipeNewInverted.classList.add("pipe-block");
-			pipeNewInverted.style.transform = "rotate(0.5turn)";
-			pipeNewInverted.style.top = pipePositionY - 70 + "vh";
-			pipeNewInverted.style.left = "100vw";
-			document.body.appendChild(pipeNewInverted);
+  function createPipes() {
+    let gameDificultyforDistance = gameDificulty * 10;
+    let pipeDistanceReference = pipeDistance - gameDificultyforDistance;
+    console.log(pipeDistanceReference);
+    if (game != "Play") return;
+    if (pipeInterval > pipeDistanceReference) {
+      console.log(gameDificulty);
+      pipeInterval = 0;
+      let randomNum = Math.floor(Math.random() * 43) + 8;
+      // console.log(randomNum);
+      let pipePositionY = randomNum;
+      let pipeNewInverted = document.createElement("img");
+      pipeNewInverted.setAttribute("src", "assets/images/pipe-green.png");
+      pipeNewInverted.classList.add("pipe-block");
+      pipeNewInverted.style.transform = "rotate(0.5turn)";
+      pipeNewInverted.style.top = pipePositionY - 70 + "vh";
+      pipeNewInverted.style.left = "100vw";
+      document.body.appendChild(pipeNewInverted);
 
-			let pipeNew = document.createElement("img");
-			pipeNew.setAttribute("src", "assets/images/pipe-green.png");
-			pipeNew.classList.add("pipe-block");
-			pipeNew.style.top = pipeOpening + pipePositionY + "vh";
-			pipeNew.style.left = "100vw";
-			document.body.appendChild(pipeNew);
-			pipeNew.increase_score = "1";
-		}
-		pipeInterval++;
-		requestAnimationFrame(createPipes);
-	}
-	requestAnimationFrame(createPipes);
+      let pipeNew = document.createElement("img");
+      pipeNew.setAttribute("src", "assets/images/pipe-green.png");
+      pipeNew.classList.add("pipe-block");
+      pipeNew.style.top = pipeOpening + pipePositionY + "vh";
+      pipeNew.style.left = "100vw";
+      document.body.appendChild(pipeNew);
+      pipeNew.increase_score = "1";
+    }
+    pipeInterval++;
+    requestAnimationFrame(createPipes);
+  }
+  requestAnimationFrame(createPipes);
 
-	/////////////////////////////////
+  /////////////////////////////////
 
-	function pipeMovement() {
-		if (game != "Play") return;
+  function pipeMovement() {
+    if (game != "Play") return;
 
-		let pipes = document.querySelectorAll(".pipe-block");
-		pipes.forEach((e) => {
-			let pipeSpecs = e.getBoundingClientRect();
-			if (pipeSpecs.right <= 0) {
-				e.remove();
-			} else {
-				if (
-					birdSpecs.right > pipeSpecs.left &&
-					birdSpecs.top < pipeSpecs.bottom &&
-					birdSpecs.bottom > pipeSpecs.top &&
-					birdSpecs.left < pipeSpecs.right
-				) {
-					console.log("Collision Over");
-					gameOver();
-					return;
-				} else {
-					if (
-						pipeSpecs.right < birdSpecs.left &&
-						pipeSpecs.right + speed >= birdSpecs.left &&
-						e.increase_score === "1"
-					) {
-						score++;
-						difficultyIncrease(score);
-						scoreBox.innerHTML = `Score: ${score}`;
-						if (highScore < score) {
-							highScore = score;
-						}
-						highScoreBox.innerHTML = `High Score: ${highScore}`;
-						// console.log(score);
-					}
-					e.style.left = pipeSpecs.left - speed + "px";
-				}
-			}
-		});
-		requestAnimationFrame(pipeMovement);
-	}
-	requestAnimationFrame(pipeMovement);
+    let pipes = document.querySelectorAll(".pipe-block");
+    pipes.forEach((e) => {
+      let pipeSpecs = e.getBoundingClientRect();
+      if (pipeSpecs.right <= 0) {
+        e.remove();
+      } else {
+        if (
+          birdSpecs.right > pipeSpecs.left &&
+          birdSpecs.top < pipeSpecs.bottom &&
+          birdSpecs.bottom > pipeSpecs.top &&
+          birdSpecs.left < pipeSpecs.right
+        ) {
+          console.log("Collision Over");
+          gameOver();
+          return;
+        } else {
+          if (
+            pipeSpecs.right < birdSpecs.left &&
+            pipeSpecs.right + speed >= birdSpecs.left &&
+            e.increase_score === "1"
+          ) {
+            score++;
+            difficultyIncrease(score);
+            scoreBox.innerHTML = `Score: ${score}`;
+            if (highScore < score) {
+              highScore = score;
+            }
+            highScoreBox.innerHTML = `High Score: ${highScore}`;
+            // console.log(score);
+          }
+          e.style.left = pipeSpecs.left - speed + "px";
+        }
+      }
+    });
+    requestAnimationFrame(pipeMovement);
+  }
+  requestAnimationFrame(pipeMovement);
 
-	/////////////////////////////////
+  /////////////////////////////////
 
-	let birdDeltaYPosition = 0;
-	function grativyEffect() {
-		if (game != "Play") return;
+  let birdDeltaYPosition = 0;
+  function grativyEffect() {
+    if (game != "Play") return;
 
-		birdDeltaYPosition = birdDeltaYPosition + gravity;
-		document.addEventListener("keydown", upMove, { once: true });
-		document.addEventListener("mousedown", upClickMove);
-		document.addEventListener("keyup", downMove, { once: true });
-		document.addEventListener("mouseup", downClickMove);
+    birdDeltaYPosition = birdDeltaYPosition + gravity;
+    document.addEventListener("keydown", upMove, { once: true });
+    document.addEventListener("mousedown", upClickMove);
+    document.addEventListener("keyup", downMove, { once: true });
+    document.addEventListener("mouseup", downClickMove);
 
-		bird.style.top = birdSpecs.top + birdDeltaYPosition + "px";
-		birdSpecs = bird.getBoundingClientRect();
-		backgroundSpecs = background.getBoundingClientRect();
-		if (birdSpecs.top < 0 || birdSpecs.bottom + speed >= backgroundSpecs.bottom) {
-			console.log("Out of Bounds Over");
-			gameOver();
-			return;
-		}
-		requestAnimationFrame(grativyEffect);
-	}
-	requestAnimationFrame(grativyEffect);
+    bird.style.top = birdSpecs.top + birdDeltaYPosition + "px";
+    birdSpecs = bird.getBoundingClientRect();
+    backgroundSpecs = background.getBoundingClientRect();
+    if (birdSpecs.top < 0 || birdSpecs.bottom + speed >= backgroundSpecs.bottom) {
+      console.log("Out of Bounds Over");
+      gameOver();
+      return;
+    }
+    requestAnimationFrame(grativyEffect);
+  }
+  requestAnimationFrame(grativyEffect);
 
-	////////////////////////////////
+  ////////////////////////////////
 
-	function upMove(e) {
-		if (e.code == "Space") {
-			bird.src = "assets/images/bluebird-downflap.png";
-			birdDeltaYPosition = -6;
-		}
-	}
-	function upClickMove() {
-		bird.src = "assets/images/bluebird-downflap.png";
-		birdDeltaYPosition = -6;
-	}
+  function upMove(e) {
+    if (e.code == "Space") {
+      bird.src = "assets/images/bluebird-downflap.png";
+      birdDeltaYPosition = -13;
+    }
+  }
+  function upClickMove() {
+    bird.src = "assets/images/bluebird-downflap.png";
+    birdDeltaYPosition = -13;
+  }
 
-	function downMove(e) {
-		if (e.code == "Space") {
-			bird.src = "assets/images/bluebird-upflap.png";
-		}
-	}
-	function downClickMove() {
-		bird.src = "assets/images/bluebird-upflap.png";
-	}
+  function downMove(e) {
+    if (e.code == "Space") {
+      bird.src = "assets/images/bluebird-upflap.png";
+    }
+  }
+  function downClickMove() {
+    bird.src = "assets/images/bluebird-upflap.png";
+  }
 
-	function difficultyIncrease(score) {
-		if (score > gameDificulty * 3) {
-			let scorePrime = score;
-			console.log(`scorePrime is ${scorePrime}`);
-			console.log(`score is ${score}`);
-			speed += 0.5;
-			gameDificulty++;
-			console.log(`difficulty is ${gameDificulty}`);
-			return;
-		}
-	}
+  function difficultyIncrease(score) {
+    if (score > gameDificulty * 3) {
+      let scorePrime = score;
+      console.log(`scorePrime is ${scorePrime}`);
+      console.log(`score is ${score}`);
+      speed += 0.75;
+      gameDificulty++;
+      console.log(`difficulty is ${gameDificulty}`);
+      return;
+    }
+  }
 
-	function gameOver() {
-		game = "Over";
-		flyBtn.classList.add("hidden");
-		bird.classList.add("hidden");
-		document.querySelector(".start-prompt").innerHTML = "Game Over!";
-		document.querySelector(".start-prompt").classList.remove("hidden");
-		let pipes = document.querySelectorAll(".pipe-block");
-		pipes.forEach((e) => {
-			let pipeSpecs = e.getBoundingClientRect();
-			e.remove();
-		});
-		playAgainBtn.classList.remove("hidden");
-		resetDataBtn.classList.remove("hidden");
-		// console.log("here");
-		return;
-	}
+  function gameOver() {
+    game = "Over";
+    flyBtn.classList.add("hidden");
+    bird.classList.add("hidden");
+    document.querySelector(".start-prompt").innerHTML = "Game Over!";
+    document.querySelector(".start-prompt").classList.remove("hidden");
+    let pipes = document.querySelectorAll(".pipe-block");
+    pipes.forEach((e) => {
+      let pipeSpecs = e.getBoundingClientRect();
+      e.remove();
+    });
+    playAgainBtn.classList.remove("hidden");
+    resetDataBtn.classList.remove("hidden");
+    // console.log("here");
+    return;
+  }
 }
